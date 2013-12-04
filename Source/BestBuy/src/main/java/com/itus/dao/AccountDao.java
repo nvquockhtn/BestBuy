41446f54ee8a775d27f87fc5a50a54343a533724
nvquockhtn <nvquockhtn@gmail.com>
2013-12-04 15:53:19 +0700

    update source

---
+++ Source/BestBuy/src/main/java/com/itus/dao/AccountDao.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.itus.dao;
+
+/**
+ *
+ * @author VanQuoc-CNTT
+ */
+public interface AccountDao {
+    
+}

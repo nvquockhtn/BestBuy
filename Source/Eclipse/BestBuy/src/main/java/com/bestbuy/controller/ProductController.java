b92e253ff3f0cf9b340f226b99e0beb0c801c0d9
vanquangtoknow <vanquangtb101@gmail.com>
2013-12-11 16:15:10 -0800

    a

--- Source/Eclipse/BestBuy/src/main/java/com/bestbuy/controller/ProductController.java
+++ Source/Eclipse/BestBuy/src/main/java/com/bestbuy/controller/ProductController.java
@@ -29,17 +29,6 @@ public class ProductController {
 	ApplicationContext context = new ClassPathXmlApplicationContext(
 			"beans-hibernate.xml");
 	ProductDao productDao = (ProductDao) context.getBean("productDao");
-	public ProductController() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	@RequestMapping(value = { "/Detail.do" }, method = RequestMethod.GET)
-	public String Detail(@RequestParam("maSP") Integer maSP, Model model) {
-
-		Product product = productDao.getProductById(maSP);
-		model.addAttribute("Product",product);
-		return "ProductDetail";
-	}
 	ProducttypeDao producttypeDao = (ProducttypeDao) context.getBean("producttypeDao");
 	ManufacturerDao manufacturerDao = (ManufacturerDao) context.getBean("manufacturerDao");
 	ArrayList<Manufacturer> listManufacturers = manufacturerDao.getListManufacturers();
@@ -49,29 +38,28 @@ public class ProductController {
 	ArrayList<Price> listEndPrices = priceDao.getListEndPrices();
 	private int pageCount = 0;
 	private int numberOnPage = 2;
+	public ProductController() {
+	}
+	
+	@RequestMapping(value = { "/Detail.do" }, method = RequestMethod.GET)
+	public String Detail(@RequestParam("maSP") Integer maSP, Model model) {
+
+		Product product = productDao.getProductById(maSP);
+		model.addAttribute("Product",product);
+		return "ProductDetail";
+	}
 	@RequestMapping(value = { "/GetProducts.do" }, method = RequestMethod.GET)
 	public String getlistProduct(
-			@ModelAttribute("product") ProductModel form,@RequestParam("page") Integer pageNumber,
-			@RequestParam("search") String search,@RequestParam("idmanufacturer") int idmanufacturer,@RequestParam("idproducttype") int idproducttype,@RequestParam("fromprice") int fromprice, @RequestParam("endprice") int endprice,
+			@ModelAttribute("product") ProductModel form,
 			Model model) {
 		
 		model.addAttribute("listManufacturers", listManufacturers);
 		model.addAttribute("listProducttypes", lisProducttypes);
 		model.addAttribute("listFromPrices", listFromPrices);
 		model.addAttribute("listEndPrices", listEndPrices);
-		model.addAttribute("idmanufacturer", idmanufacturer);
-		model.addAttribute("idproducttype", idproducttype);
-		model.addAttribute("fromprice", fromprice);
-		model.addAttribute("endprice", endprice);
-		model.addAttribute("search", search);
-		form.setManufacturer(idmanufacturer);
-		form.setProducttype(idproducttype);
-		form.setFromprice(fromprice);
-		form.setEndprice(endprice);
-		form.setName(search);
 		pageCount = 0;
-		int pageCountTemp = productDao.searchProductBy(form.getName(), form.getManufacturer(),
-				form.getProducttype(), form.getFromprice(), form.getEndprice(), form.getProductstate(), pageNumber, numberOnPage);
+		int pageCountTemp = productDao.searchProductBy(form.getSearch(), form.getIdmanufacturer(),
+				form.getIdproducttype(), form.getFromprice(), form.getEndprice(), form.getIdproductstate(), form.getPage(), numberOnPage);
 		model.addAttribute("listProducts", BestBuyHelperDao.listProducts);
 		if(pageCountTemp>0)
         {
@@ -85,46 +73,7 @@ public class ProductController {
             }
         }
 		model.addAttribute("pageCount",pageCount );
-		model.addAttribute("pageNumber", pageNumber);
-		
 		return "Product";
 	}
-	@RequestMapping(value = { "/PostProducts.do" }, method = RequestMethod.POST)
-	public String postlistProduct(
-			@ModelAttribute("product") ProductModel form,@RequestParam("page") Integer pageNumber,
-			@RequestParam("search") String search,@RequestParam("idmanufacturer") int idmanufacturer,@RequestParam("idproducttype") int idproducttype,@RequestParam("fromprice") int fromprice, @RequestParam("endprice") int endprice,
-			Model model) {
-		model.addAttribute("listManufacturers", listManufacturers);
-		model.addAttribute("listProducttypes", lisProducttypes);
-		model.addAttribute("listFromPrices", listFromPrices);
-		model.addAttribute("listEndPrices", listEndPrices);
-		if(form.getName()==null)
-		{
-			form.setName("");
-		}
-		model.addAttribute("idmanufacturer", form.getManufacturer());
-		model.addAttribute("idproducttype", form.getProducttype());
-		model.addAttribute("fromprice", form.getFromprice());
-		model.addAttribute("endprice", form.getEndprice());
-		model.addAttribute("search", form.getName());
-		int pageCountTemp = productDao.searchProductBy(form.getName(), form.getManufacturer(),
-				form.getProducttype(), form.getFromprice(), form.getEndprice(), form.getProductstate(), pageNumber, numberOnPage);
-		model.addAttribute("listProducts", BestBuyHelperDao.listProducts);
-		pageCount = 0;
-		if(pageCountTemp>0)
-        {
-            if(pageCountTemp%numberOnPage==0)
-            {
-                pageCount =  (int)pageCountTemp/numberOnPage;
-            }
-            else
-            {
-                pageCount =  ((int)pageCountTemp/numberOnPage) +1;
-            }
-        }
-		model.addAttribute("pageCount",pageCount );
-		model.addAttribute("pageNumber", pageNumber);
-		
-		return "Product";
-	}
+	
 }
